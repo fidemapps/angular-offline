2e0a43f20de18ba58aa8611d13d514e6760eaa4b
cgu <cyrille@cyrgue.com>
2015-08-28 08:35:37 -0400

    Allow url with params as cache key

--- angular-offline.js
+++ angular-offline.js
@@ -173,6 +173,34 @@ angular
         });
     }
 
+    function encodeUriQuery(val, pctEncodeSpaces) {
+      return encodeURIComponent(val).
+        replace(/%40/gi, '@').
+        replace(/%3A/gi, ':').
+        replace(/%24/g, '$').
+        replace(/%2C/gi, ',').
+        replace(/%20/g, (pctEncodeSpaces ? '%20' : '+'));
+    }
+
+    function buildUrl(url, params) {
+      if (!params) return url;
+      var parts = [];
+      angular.forEach(params, function(value, key) {
+        if (value === null || angular.isUndefined(value)) return;
+        if (!angular.isArray(value)) value = [value];
+
+        angular.forEach(value, function(v) {
+          if (angular.isObject(v)) {
+            v = angular.toJson(v);
+          }
+          parts.push(encodeUriQuery(key) + '=' + encodeUriQuery(v));
+        });
+      });
+      return url + ((url.indexOf('?') == -1) ? '?' : '&') + parts.join('&');
+    }
+
+
+
     /**
      * Process all the stack.
      *
@@ -232,7 +260,7 @@ angular
         if (config.method === 'GET') {
           // Online we clean the cache.
           if (connectionStatus.isOnline())
-            cleanIfExpired(config.cache, config.url);
+            cleanIfExpired(config.cache, buildUrl(config.url, config.params));
 
           return config;
         }
